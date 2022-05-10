bb66781468460a14852c7ab600f20c1eb5568aa1
MuxtorbekDev <muxtorbekravshanov9@gmail.com>
2022-05-10 10:15:45 +0500

    add notebook

--- routes/add.js
+++ routes/add.js
@@ -1,8 +1,16 @@
 const { Router } = require("express");
+const Notebook = require("../models/notebook");
 const router = Router();
 
 router.get("/", (req, res) => {
   res.render("add", { title: "Add Page", isAdd: true });
+});
+
+router.post("/", async (req, res) => {
+  console.log(req.body);
+  const notebook = new Notebook(req.body.title, req.body.price, req.body.img);
+  await notebook.save();
+  res.redirect("/notebooks");
 });
 
 module.exports = router;
