90590b0727bc7669299db896f3272817b6174aea
MuxtorbekDev <muxtorbekravshanov9@gmail.com>
2022-05-12 12:14:15 +0500

    empty

--- public/js/app.js
+++ public/js/app.js
@@ -1,14 +1,23 @@
-const priceNotebook = document.querySelectorAll(".price");
+// const priceNotebook = document.querySelectorAll(".price");
 
-priceNotebook.forEach((item) => {
-  item.textContent = new Intl.NumberFormat("us-Us", {
+// priceNotebook.forEach((item) => {
+//   item.textContent = new Intl.NumberFormat("us-Us", {
+//     currency: "USD",
+//     style: "currency",
+//   }).format(item);
+// });
+const toCurrency = (price) => {
+  return new Intl.NumberFormat("us-US", {
     currency: "USD",
     style: "currency",
-  }).format(item.textContent);
+  }).format(price);
+};
+
+document.querySelectorAll(".price").forEach((m) => {
+  m.textContent = toCurrency(m.textContent);
 });
 
 const $card = document.querySelector("#card");
-
 if ($card) {
   $card.addEventListener("click", (e) => {
     if (e.target.classList.contains("js-remove")) {
@@ -18,7 +27,7 @@ if ($card) {
       })
         .then((res) => res.json())
         .then((card) => {
-          if (card.notebooks.length === 0) {
+          if (card.notebooks.length) {
             const dynamicHTML = card.notebooks
               .map((c) => {
                 return `
@@ -33,9 +42,34 @@ if ($card) {
               })
               .join("");
             $card.querySelector("tbody").innerHTML = dynamicHTML;
-            $card.querySelector(".price").textContent = card.price;
+            $card.querySelector(".price").textContent = toCurrency(card.price);
           } else {
-            $card.innerHTML = "<b>Basket is empty</b>";
+            $card.innerHTML = `
+            <div class="row">
+				 
+					<div class="col-md-12">
+					
+							<div class="card">
+						<div class="card-header">
+						<h5>Cart</h5>
+						</div>
+						<div class="card-body cart">
+								<div class="col-sm-12 empty-cart-cls text-center">
+									<img src="https://i.imgur.com/dCdflKN.png" width="130" height="130" class="img-fluid mb-4 mr-3">
+									<h3><strong>Your Cart is Empty</strong></h3>
+									<h4>Add something to make me happy :)</h4>
+									<a href="/notebooks" class="btn btn-primary cart-btn-transform m-3" data-abc="true">continue shopping</a>
+									
+								
+								</div>
+						</div>
+				</div>
+						
+					
+					</div>
+				 
+				 </div>
+            `;
           }
         });
     }
