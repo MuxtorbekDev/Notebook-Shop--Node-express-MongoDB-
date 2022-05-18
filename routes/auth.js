5394fccc1cca64fed408966a6380715daef2b0a8
MuxtorbekDev <muxtorbekravshanov9@gmail.com>
2022-05-18 16:35:21 +0500

    bcryptjs hash

--- routes/auth.js
+++ routes/auth.js
@@ -1,68 +1,73 @@
 const { Router } = require("express");
 const router = Router();
 const User = require("../models/user");
+const bcrypt = require("bcryptjs");
 
 router.get("/login", (req, res) => {
-    res.render("auth/login", {
-        title: "Register",
-        isLogin: true,
+  res.render("auth/login", {
+    title: "Register",
+    isLogin: true,
+  });
+});
+
+router.get("/logout", async (req, res) => {
+  try {
+    req.session.destroy(() => {
+      res.redirect("/auth/login#login");
     });
+  } catch (e) {
+    console.log(e);
+  }
 });
 
-router.get("/logout", async(req, res) => {
-    try {
-        req.session.destroy(() => {
-            res.redirect("/auth/login#login");
+router.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const candidate = await User.findOne({ email });
+    console.log(candidate);
+    if (candidate) {
+      const userPas = bcrypt.compare(password, candidate.password);
+      if (userPas) {
+        req.session.user = candidate;
+        req.session.isAuthenticated = true;
+        req.session.save((err) => {
+          console.log(err);
+          if (err) throw err;
+          res.redirect("/");
         });
-    } catch (e) {
-        console.log(e);
+      }
+    } else {
+      res.redirect("/auth/login#login");
     }
+  } catch (e) {
+    console.log(e);
+  }
 });
 
-router.post("/login", async(req, res) => {
-    try {
-        const { email, password } = req.body;
-        const candidate = await User.findOne({ email });
-        console.log(candidate);
-        if (candidate) {
-            const userPas = password === candidate.password;
-            if (userPas) {
-                req.session.user = candidate;
-                req.session.isAuthenticated = true;
-                req.session.save((err) => {
-                    console.log(err);
-                    if (err) throw err;
-                    res.redirect("/");
-                });
-            }
-        } else {
-            res.redirect("/auth/login#login");
-        }
-    } catch (e) {
-        console.log(e);
-    }
-});
+router.post("/register", async (req, res) => {
+  try {
+    const { email, name, password } = req.body;
 
-router.post("/register", async(req, res) => {
-    try {
-        const { email, name, password } = req.body;
-        const candidate = await User.findOne({ email });
-        if (candidate) {
-            res.redirect("/auth/login#register");
-        } else {
-            const user = new User({
-                email: email,
-                name: name,
-                password: password,
-                cart: { items: [] },
-            });
+    const candidate = await User.findOne({ email });
 
-            await user.save();
-            res.redirect("/auth/login#login");
-        }
-    } catch (e) {
-        console.log(e);
+    if (candidate) {
+      res.redirect("/auth/login#register");
+    } else {
+      const hashPass = await bcrypt.hash(password, 12);
+      console.log(hashPass);
+      const user = new User({
+        email: email,
+        name: name,
+        password: hashPass,
+        cart: { items: [] },
+      });
+
+      await user.save();
+      res.redirect("/auth/login#login");
     }
+  } catch (e) {
+    console.log(e);
+  }
 });
 
 module.exports = router;
